01666233a88d2a4f55e6ec99b99aee3c193eed82
Brock Tubre <brocktubre@gmail.com>
2020-12-28 10:54:58 -0500

    Initial Commit

---
+++ RandomWorkoutSandbox/RandomWorkoutSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWorkoutSandboxApp.swift
+//  RandomWorkoutSandbox
+//
+//  Created by Brock Tubre on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct RandomWorkoutSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
